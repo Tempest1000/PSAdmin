b3b0cf04b2597d64059eef09c83f18af628d30b7
Andrew Baines <andrew.baines@argodata.com>
2017-05-25 16:23:26 -0500

    Add css concat, bootstrap, and lint.

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,21 @@ var open = require('gulp-open'); // opens a URL in a web browser
 var browserify = require('browserify'); // bundles JS
 var reactify = require('reactify'); // tranforms React JSX to JS
 var source = require('vinyl-source-stream'); // use conventional text streams with gulp
+var concat = require('gulp-concat'); // concatenates files
+var lint = require('gulp-eslint'); // lint js files, including jsx
 
 var config = {
     port: 9005,
     devBaseUrl: 'http://localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 }
 
@@ -39,9 +47,32 @@ gulp.task('html', function () {
         .pipe(connect.reload());
 });
 
+gulp.task('js', function () {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
+gulp.task('css', function() {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('lint', function () {
+    return gulp.src(config.paths.js)
+        .pipe(lint({config: 'eslint.config.json'}))
+        .pipe(lint.format());
+});
+
 // watch for file changes
 gulp.task('watch', function () {
     gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
 });
 
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
